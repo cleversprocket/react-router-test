6961d91ef0e80fd3df7131c894bac9e7508f2781
Matt Morrow <matt.morrow@nordstrom.com>
2019-09-10 15:21:05 -0700

    Trying to fix back/forward

--- src/store/router-transition-middleware.js
+++ src/store/router-transition-middleware.js
@@ -1,55 +1,55 @@
 import { CALL_HISTORY_METHOD, LOCATION_CHANGE } from "connected-react-router";
 import loadRouteData from "../load-route-data";
 import { matchRoutes } from "react-router-config";
-// import {matchPath} from "react-router-dom";
 import routes from "../routes";
+
+// if (global.document) {
+//     window.onpopstate = function (event) {
+//         console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+//     };
+
+// }
 
 const routerTransitionMiddleware = history => store => next => async action => {
     console.log("intercepted connected-react-router. action: ", action);
     const {
         type, 
         payload: {
-            action: navAction
+            args,
+            action: browserAction,
+            method: historyMethod,
+            isFirstRendering,
+            location: {
+                pathname
+            } = {}
         } = {}
     } = action;
-
-    let passThrough = true;
-
-    if (type === CALL_HISTORY_METHOD) {
-        passThrough = false;
-    } else if (type === LOCATION_CHANGE && navAction === "POP") {
-        passThrough = false;
-    }
-
-
-
-    if (passThrough) {
-        console.log("Passing " + type + " through via " + navAction);
-        return next(action);
-    }
-
-    console.log("NOT Passing " + type + " through via " + navAction);
-    console.log("---------------------------------------------------------");
-    console.log("");
-
-
-    const {
-        payload,
-        payload: {
-            location: {
-                args,
-                pathname,
-            } = {},
-            action: historyAction,
-            isFirstRendering
-        } = {}
-    } = action || {};
-
     const {
         dispatch
     } = store;
-    const toRoute = pathname || payload.args[0];
- 
+
+    let passThrough = true;
+    const navAction = historyMethod || browserAction;
+
+    if (!isFirstRendering)  {
+        if (type === CALL_HISTORY_METHOD) {
+            passThrough = false;
+        } else if (type === LOCATION_CHANGE && navAction.toLowerCase() === "pop") {
+            passThrough = false;
+        }
+    }
+
+    if (passThrough) {
+        console.log("Passing " + type + " through via " + navAction + " for " + pathname);
+        next(action);
+        return
+    }
+
+    console.log("NOT Passing " + type + " through via " + navAction + " for " + pathname);
+    console.log("---------------------------------------------------------");
+    console.log("");
+
+    const toRoute = pathname || args[0]; 
     const matchingRoutes = matchRoutes(routes, toRoute);
 
     await Promise.all(loadRouteData(matchingRoutes, dispatch));
